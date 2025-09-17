f7faa51df902426af2664398bfa5583d1591925a
Michael Freimüller <michael@freimueller.com>
2025-09-17 17:57:31 +0200

    initial commit

---
+++ src/main/java/com/mfreimueller/springrader/SprinGraderApplication.java
@@ -0,0 +1,13 @@
+package com.mfreimueller.springrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinGraderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinGraderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mfreimueller/springrader/SprinGraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfreimueller.springrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinGraderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
