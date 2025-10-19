bcdc5ceccc2c5f6c0b7f21e2ece0ac7b96cadc61
Michael Freimüller <michael@mfreimueller.com>
2025-10-19 17:53:04 +0200

    implement mode converter

---
+++ src/main/java/com/mfreimueller/art/persistence/converters/ModeConverter.java
@@ -0,0 +1,30 @@
+package com.mfreimueller.art.persistence.converters;
+
+import com.mfreimueller.art.domain.Creator;
+import com.mfreimueller.art.domain.SlideshowContent;
+import com.mfreimueller.art.exceptions.DataConstraintException;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class ModeConverter implements AttributeConverter<SlideshowContent.Mode, Character> {
+    @Override
+    public Character convertToDatabaseValue(SlideshowContent.Mode mode) {
+        return switch (mode) {
+            case null -> null;
+            case SlideshowContent.Mode.Auto -> 'a';
+            case SlideshowContent.Mode.Hybrid -> 'h';
+            case SlideshowContent.Mode.Manual -> 'm';
+        };
+    }
+
+    @Override
+    public SlideshowContent.Mode convertToEntity(Character character) {
+        return switch (character) {
+            case null -> null;
+            case 'a' -> SlideshowContent.Mode.Auto;
+            case 'h' -> SlideshowContent.Mode.Hybrid;
+            case 'm' -> SlideshowContent.Mode.Manual;
+            default -> throw DataConstraintException.forUnmappedEnumValue(character, Creator.Role.class);
+        };
+    }
+}
---
+++ src/test/java/com/mfreimueller/art/persistence/converters/ModeConverterTest.java
@@ -0,0 +1,41 @@
+package com.mfreimueller.art.persistence.converters;
+
+import com.mfreimueller.art.domain.SlideshowContent;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class ModeConverterTest {
+
+    private final ModeConverter converter = new ModeConverter();
+
+    @ParameterizedTest
+    @MethodSource("mappingData")
+    void can_convert_to_db_value_and_back(SlideshowContent.Mode mode, Character dbValue) {
+        var result = converter.convertToDatabaseValue(mode);
+        assertThat(result).isEqualTo(dbValue);
+        assertThat(converter.convertToEntity(result)).isEqualTo(mode);
+    }
+
+    @Test
+    void can_convert_null_values_safely() {
+        assertThat(converter.convertToEntity(null)).isNull();
+        assertThat(converter.convertToDatabaseValue(null)).isNull();
+    }
+
+    private static Stream<Arguments> mappingData() {
+        return Stream.of(
+                Arguments.of(SlideshowContent.Mode.Auto, 'a'),
+                Arguments.of(SlideshowContent.Mode.Hybrid, 'h'),
+                Arguments.of(SlideshowContent.Mode.Manual, 'm')
+        );
+    }
+}
