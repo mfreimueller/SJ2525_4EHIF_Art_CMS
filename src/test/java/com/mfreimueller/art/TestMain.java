b532a58351e7bf7539328e255bfd0c6cd2bc3998
Michael Freimüller <michael@mfreimueller.com>
2025-11-06 10:05:13 +0100

    add test container (for postgres) config and adapt first test

---
+++ src/test/java/com/mfreimueller/art/TestMain.java
@@ -0,0 +1,39 @@
+package com.mfreimueller.art;
+
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.HostConfig;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@Configuration
+public class TestMain {
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> testMainPostgresContainer() {
+        final Integer containerPort = 5432;
+
+        PortBinding portBinding = new PortBinding(Ports.Binding.bindPort(15432), new ExposedPort(containerPort));
+
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:17.6-alpine"))
+                .withCreateContainerCmdModifier(cmd -> {
+                    cmd.withName("pg.sj2526.4ehif");
+                    cmd.withHostConfig(new HostConfig().withPortBindings(portBinding));
+                })
+                .withDatabaseName("pg2526_4ehif")
+                .withUsername("owner")
+                .withPassword("pwd")
+                .withReuse(true);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(ArtCmsApplication::main)
+                .with(TestMain.class)
+                .run(args);
+    }
+}
--- src/test/java/com/mfreimueller/art/persistence/AudioContentRepositoryTest.java
+++ src/test/java/com/mfreimueller/art/persistence/AudioContentRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.mfreimueller.art.persistence;
 
+import com.mfreimueller.art.TestcontainersConfiguration;
 import com.mfreimueller.art.domain.Creator;
 import com.mfreimueller.art.domain.AudioContent;
 import com.mfreimueller.art.domain.Language;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.time.ZonedDateTime;
 import java.util.Map;
@@ -17,6 +19,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@Import(TestcontainersConfiguration.class)
 class AudioContentRepositoryTest {
 
     private @Autowired AudioContentRepository audioContentRepository;
