eb6a657ef79643daf5ec5daf0f241e503b3f0f77
Michael Freimüller <michael@mfreimueller.com>
2025-11-14 17:05:43 +0100

    adjust key type of ExhibitionRepository to CollectionId

--- src/main/java/com/mfreimueller/art/persistence/ExhibitionRepository.java
+++ src/main/java/com/mfreimueller/art/persistence/ExhibitionRepository.java
@@ -1,10 +1,11 @@
 package com.mfreimueller.art.persistence;
 
+import com.mfreimueller.art.domain.Collection;
 import com.mfreimueller.art.domain.Exhibition;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExhibitionRepository extends JpaRepository<Exhibition, Long> {
+public interface ExhibitionRepository extends JpaRepository<Exhibition, Collection.CollectionId> {
 
 }
